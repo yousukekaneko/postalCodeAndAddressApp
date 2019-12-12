280822cfa6231746fb328f231a5545c503ae32a8
yousukekaneko <kaneko1987-0402@i.softbank.jp>
2019-12-12 21:31:37 +0900

    改行の修正、インデントの修正

--- app/src/main/java/com/example/android/sample/postalcodeandaddressapp/AddressResponse.kt
+++ app/src/main/java/com/example/android/sample/postalcodeandaddressapp/AddressResponse.kt
@@ -1,7 +1,13 @@
 package com.example.android.sample.postalcodeandaddressapp
 
 data class AddressResponse (
-    val prefecture : String?,
-    val city : String?,
-    val town : String?
+
+    val city : String,
+    val city_kana: String,
+    val town : String,
+    val town_kana: String,
+    val latitude: String,
+    val longitude: String,
+    val prefecture : String,
+    val postal: String
 )
