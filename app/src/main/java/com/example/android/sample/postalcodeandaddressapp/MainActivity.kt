0cd30a513c26c639b34f36fed8842bfcf87aaf98
yousukekaneko <kaneko1987-0402@i.softbank.jp>
2019-12-22 20:24:14 +0900

    mainActivityにclientクラスの内容反映 クリック処理（簡易）を実装

--- app/src/main/java/com/example/android/sample/postalcodeandaddressapp/MainActivity.kt
+++ app/src/main/java/com/example/android/sample/postalcodeandaddressapp/MainActivity.kt
@@ -2,7 +2,11 @@ package com.example.android.sample.postalcodeandaddressapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-
+import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +15,37 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        //log interceptorの設定
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+        val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
+
+        //retrofitインスタンスを生成
+        val retrofit = Retrofit.Builder()
+            .baseUrl("http://geoapi.heartrails.com")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+
+        val service = retrofit.create(HeartRailsService::class.java)
+
+        val addressResponse = service.apiGet("", "")
+
+
+
+        //クリック処理（簡易）
+        searchButton.setOnClickListener {
+            val inputAddress = textPostalAddress.text.toString()
+            resultLabel.text = inputAddress
+
+            //TODO 入力した値が正しいかどうかの処理
+
+            //TODO 正しい場合、fun getApiを呼ぶ
+
+            //TODO 誤っている場合、アラートを出す
+
+
+        }
     }
 }
